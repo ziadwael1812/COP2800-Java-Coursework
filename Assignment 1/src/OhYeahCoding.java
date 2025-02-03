a87d35117a1542da087135b4a3e88247cd966ab7
ziadwael1812 <ziadwael1812@gmail.com>
2025-02-03 02:43:52 -0500

    Assinment 1.1

---
+++ Assignment 1/src/OhYeahCoding.java
@@ -0,0 +1,12 @@
+// Author: Ziad Aboalmagd
+// Course: COP 2800
+// Assignment: Exercise 1.1
+
+public class OhYeahCoding {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
