780e748aa6b104072c6def5740454f328fd1e641
ziadwael1812 <ziadwael1812@gmail.com>
2025-02-04 15:13:09 -0500

    1.2

---
+++ Module 1/Exercise 1.2/loops.java
@@ -0,0 +1,12 @@
+// Author: Ziad Aboalmagd
+// Course: COP 2800
+// Assignment: Exercise 1.2
+
+public class loops {
+
+    public static void main(String[] args) {
+        for (int i=0; i<5; i++) {
+            System.out.println("Welcome to Java");
+        }
+    }
+}
